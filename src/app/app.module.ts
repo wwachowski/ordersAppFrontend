4266e4e69f445dde0ce3a3a460bb9fce38e2c720
wwachowski <wiktorwachowski008@gmail.com>
2022-06-04 15:32:04 +0200

    refactor: refactor material imports/exports in one module (material-module)
    Create new module with materials to import to app-module instead of separate imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './materials/material/material.module';
 
 @NgModule({
   declarations: [
@@ -15,9 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
+    MaterialModule,
     BrowserAnimationsModule
   ],
   providers: [],
