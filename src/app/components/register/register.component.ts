1a8cb7c558f7d5823071f3bf8089befe45c3cffc
wwachowski <wiktorwachowski008@gmail.com>
2022-06-07 20:17:08 +0200

    feat(register): features same as the last commit respectively - register component
    TODO: Enchance validation and information for user under specific circumstances

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
-import { User } from 'src/app/data/models/user';
 
 @Component({
   selector: 'app-register',
@@ -9,32 +9,32 @@ import { User } from 'src/app/data/models/user';
 })
 export class RegisterComponent implements OnInit {
 
-  username: string = '';
-  password: string = '';
-  passwordRetyped: string = '';
+  @ViewChild('userForm') form!: NgForm;
 
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    this.register();
+  }
+
   register(): void {
     if (this.registerVerify()) {
-      this.userService.register(this.username, this.password)
+      this.userService.register(this.form.value.username, this.form.value.password)
         .subscribe({
           complete: () => { alert("Completed") },
           error: (err) => { alert(err) },
           next: (res) => { alert(res) }
         });
-    } else {
-      //TODO: display message error 
     }
   }
 
   registerVerify(): boolean {
-    const username = this.username.trim();
-    if (username.length && this.password.length) {
-      return this.password === this.passwordRetyped;
+    const username = this.form.value.username.trim();
+    if (username.length && this.form.value.password) {
+      return this.form.value.password === this.form.value.passwordRetyped;
     } else return false;
   }
 }
